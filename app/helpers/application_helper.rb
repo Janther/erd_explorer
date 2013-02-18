a886f13a7c17bb8cf0d881e9cd114c8b28f8ed43
Klaus Hott Vidal <klahott@gmail.com>
2013-02-17 22:43:45 -0300

    Added extra details for the attribute Model

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,15 @@
 module ApplicationHelper
+  def purpose_for attribute
+    if attribute.primary_key?
+      "primary_key"
+    elsif attribute.foreign_key?
+      "foreign_key"
+    elsif attribute.timestamp?
+      "timestamp"
+    elsif attribute.inheritance?
+      "inheritance"
+    elsif attribute.content?
+      "content"
+    end
+  end
 end
